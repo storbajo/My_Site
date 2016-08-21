51c81b44417c429df4618b7e20f2988c602f6b95
Javier Vargas <javvarga@cobogpgp1085.global.publicisgroupe.net>
2016-08-21 13:17:06 -0500

    adding font icon

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-$("#nav-icon").click(function(){
-	$(this).parents().find("body").toggleClass("open");
+$(document).ready(function(){
+	var body = $("body"),
+		bodyWidth = body.width();
+
+	$(window).on("resize", function() {
+		if( bodyWidth > 1024 && body.hasClass("open")) {
+			body.toggleClass("open");
+		}
+	});
+
+	$("#nav-icon").click(function(){
+		$(this).parents().find("body").toggleClass("open");
+	});
 });
+
+
+
