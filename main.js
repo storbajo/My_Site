cb03c9e2cc7266b2f7ddfa818cca13a16781573f
Javier Vargas <javvarga@cobogpgp1085.global.publicisgroupe.net>
2016-08-21 20:35:47 -0500

    config landing page

--- main.js
+++ main.js
@@ -6,12 +6,47 @@ $(document).ready(function(){
 		if( bodyWidth > 1024 && body.hasClass("open")) {
 			body.toggleClass("open");
 		}
+
+		if(bodyWidth < 1024) {
+			$(".main").moveTo(1);
+		}
+	});
+
+	$(".nav-anchor").on('click', function(){
+		if(bodyWidth < 1024 && body.hasClass("open")) {
+			$(this).parents().find(body).removeClass('open');
+		}
 	});
 
 	$("#nav-icon").click(function(){
 		$(this).parents().find("body").toggleClass("open");
 	});
+
+	$(".nav-anchor").on('click', function() {
+		$(".nav-anchor").removeClass('active');
+		var currentTarget = $(this).attr('data-index');
+		$(this).moveTo(currentTarget);
+		$(this).addClass("active");
+		$(".onepage-wrapper .section").scrollTop(0);
+	});
+
+	$(".logo").on('click', function() {
+		$(this).moveTo(1);
+		body.removeClass("open");
+	});
+
+	$(".work-item .image").each(function(){
+   		var bgImg = $(this).data("image");
+   		if($("html").hasClass("ie8")){
+   			$(this).append("<img src='" + bgImg  +"'>")
+   		} else{
+			$(this).css("background-image", "url('" + bgImg + "')");
+		}
+	});
+
+	$(".main").onepage_scroll();
 });
+
 
 
 
